eb3a8024d542a5b5886c746e89906cfd4745547b
Bruno Costa Candia <bruno852812@gmail.com>
2022-05-22 02:08:45 -0300

    Images Upload

---
+++ src/components/HorseCarrossel/HorseCarrossel.svelte
@@ -0,0 +1,17 @@
+<script>
+  import ArrowLeftCircleIcon from "../Icons/ArrowLeftCircleIcon.svelte";
+  import ArrowRightCircleIcon from "../Icons/ArrowRightCircleIcon.svelte";
+  import "../HorseCarrossel/style.scss";
+</script>
+
+<div class='horse-content'>
+  <button>
+    <ArrowLeftCircleIcon />
+  </button>
+  <div class="horse-image">
+    <img src="/images/8C.png" alt="Cavalo de raça 8A" />
+  </div>
+  <button>
+    <ArrowRightCircleIcon />
+  </button>
+</div>
--- src/routes/horse/corcel.svelte
+++ src/routes/horse/corcel.svelte
@@ -1,37 +1,40 @@
 <script>
-  import Button from '../../components/Button/Button.svelte';
-  import '../horse/style.scss';
-</script> 
+  import HorseCarrossel from "../../components/HorseCarrossel/HorseCarrossel.svelte";
+  import Button from "../../components/Button/Button.svelte";
+  import "../horse/style.scss";
+</script>
 
-<section class='tier'>
-    <h1 class='title'>TIER</h1>
-    <div class='tiers'>
-      {#each [8,7,6,5] as j}
-        <Button tier={j} />
-      {/each}
-    </div>
-    <div class='content'>
-      <div class='horse-content'>
-        <img src='/images/8C.png' alt="Cavalo de raça 8A">
-      </div>
+<section class="tier">
+  <h1 class="title">TIER</h1>
+  <div class="tiers">
+    {#each [5, 6, 7, 8] as j}
+      <Button tier={j} />
+    {/each}
+  </div>
+  <div class="content">
+    <HorseCarrossel />
+    <div>
       <div>
+        <h2>Nível</h2>
+        <input type="number" />
+        <h2>Habilidade</h2>
+        <input type="number" />
         <div>
-          <h2>Nível</h2>
-          <input type="number">
-          <h2>Habilidade</h2>
-          <input type="number">
-          <div>
-            <h2>Uso de Cupom</h2>
-            <img src='/images/forte-coice.png' alt="Cavalo de raça 8A">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien sit ligula vulputate mi cras nisl dapibus facilisis dolor.</span>
-          </div>
-          <div>
-            <h2>Ordem de Troca de Habilidade</h2>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien sit ligula vulputate mi cras nisl dapibus facilisis dolor.</span>
-          </div>
-
+          <h2>Uso de Cupom</h2>
+          <img src="/images/forte-coice.png" alt="Cavalo de raça 8A" />
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien sit
+            ligula vulputate mi cras nisl dapibus facilisis dolor.</span
+          >
         </div>
-
+        <div>
+          <h2>Ordem de Troca de Habilidade</h2>
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien sit
+            ligula vulputate mi cras nisl dapibus facilisis dolor.</span
+          >
+        </div>
       </div>
     </div>
+  </div>
 </section>
